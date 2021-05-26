8eb9ee5eb0ed1facc67281a73396f4e4b75afa4c
hp <chhabraashish123@gmail.com>
2021-05-26 17:46:08 +0530

    Class two added

---
+++ ClassTwo.java
@@ -0,0 +1,114 @@
+package skilligence;
+
+import java.util.Scanner;
+
+public class ClassTwo {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter the character?");
+//		char ch = s.next().charAt(0);
+//		switch (ch) {
+//		case 'a':
+//			System.out.println("Is a vowel");
+//			break;
+//
+//		case 'e':
+//			System.out.println("Is a vowel");
+//			break;
+//
+//		case 'i':
+//			System.out.println("Is a vowel");
+//			break;
+//
+//		case 'o':
+//			System.out.println("Is a vowel");
+//			break;
+//
+//		case 'u':
+//			System.out.println("Is a vowel");
+//			break;
+//
+//		default:
+//			System.out.println("Is a consonant");
+//
+//		}
+
+		// LOOPS
+		// 1. For loop
+
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+//		System.out.println("Hello");
+
+		// 5*1=5
+		// 5*2=10
+
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println("5 * " + i + " = " + 5 * i);
+//		}
+
+		// 4 - 10 (4+3+2+1)
+
+		// Factorial
+//		int f=1;
+//		System.out.println("Enter the number?");
+//		int n=s.nextInt();
+//		for(int i=1;i<=n;i++)
+//			f=f*i;
+//		System.out.println(f);
+
+		// Reverse For Loop
+
+//		for(int i=10;i>=1;i--)
+//			System.out.println("Hello");
+
+		// Fibonacci series
+//		System.out.println("Enter the number?");
+//		int n=s.nextInt();
+//		int a=0,b=1,c;
+//		for(int i=2;i<=n;i++) {
+//			c=a+b;
+//			a=b;
+//			b=c;
+//		}
+//		System.out.println(b);
+
+		// WHILE LOOP
+//		int i=9;
+//		int sum=0;
+//		while(i>0) {
+//			sum=sum+i;
+//			i--;
+//		}
+//		System.out.println(sum);
+
+//		int i = 0;
+//		for (; i < 10;) {
+//			System.out.println("hello");
+//			i++;
+//		}
+		
+		
+		//DO WHILE LOOP
+//		for(int j=0;j<0;j++)
+//			System.out.println("Hello");
+//		
+//		int k=0;
+//		while(k<0) {
+//			System.out.println("Hello");
+//			k++;
+//		}
+//		
+//		int i=0;
+//		do {
+//			System.out.println("Hello");
+//			i++;
+//		}while(i<0);
+		
+		
+	}
+
+}
