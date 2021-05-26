5ca77eb3777bcb165cb32abe83662883643a1b3d
hp <chhabraashish123@gmail.com>
2021-05-26 17:42:02 +0530

    class one added

---
+++ Classone.java
@@ -0,0 +1,139 @@
+package skilligence;
+
+import java.util.Scanner;
+
+public class Classone {
+
+	public static void main(String[] args) {
+		// output
+//		System.out.println("Hello Students");
+//		System.out.print("Hello Students");
+//		System.out.println();
+//		System.out.print("Hello Students");
+//		System.out.print("Hello Students");
+
+//		System.out.print("hello\n");
+//		System.out.print("hello");
+
+//		System.out.println(3);
+//		System.out.print(4);
+//		System.out.print(5);
+//		System.out.println();
+//		System.out.print(6);
+//		System.out.print(7);
+
+		// input
+		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter your name?");
+//		String s = sc.nextLine();
+//		System.out.println("My name is: " + s);
+
+//		System.out.println("Enter your age?");
+//		int age = sc.nextInt();
+//		System.out.println("My age is: " + age);
+
+//		float marks=sc.nextFloat();
+//		System.out.println(marks);
+
+//		double marks=sc.nextDouble();
+//		System.out.println(marks);
+
+		// swapping two variables with using third variable
+//		System.out.println("Enter first value");
+//		int a=sc.nextInt();
+//		System.out.println("Enter Second Value");
+//		int b=sc.nextInt();
+//		
+//		
+//		System.out.println("Before Swapping");
+//		System.out.println(a);
+//		System.out.println(b);
+
+//		swap two values
+//		int temp=a;
+//		a=b;
+//		b=temp;
+//		
+//		System.out.println("After Swapping");
+//		System.out.println(a);
+//		System.out.println(b);
+
+		// Unary and Binary OPERATORS
+//		System.out.println(5 / 2); // quotient
+//		System.out.println(5 % 2);// remainder
+
+		// Pre_Increment - ++variable
+//		int a = 7;
+//		System.out.println(++a);
+
+		// Post-Increment - variable++
+//		System.out.println(a++);
+//		System.out.println(a);
+
+		// Pre-decrement - --variable
+//		System.out.println(--a);
+
+		// Post-decrement- variable--
+//		System.out.println(a--);
+
+//		System.out.println(++a); //8
+//		System.out.println(a--); //8 
+//		System.out.println(a);  //7
+//		System.out.println(--a); //6
+//		System.out.println(a);  //6
+//		System.out.println(a++); //6
+//		System.out.println(a);//7
+
+		// COMPARATIVE OPERATORS
+
+//		int a = 5, b = 5;
+//		System.out.println(a > b); //false
+//		System.out.println(a < b); //false
+//		System.out.println(a >= b);// true
+//		System.out.println(a <= b);// true
+//		System.out.println(a == b);// true
+//		System.out.println(a != b);// false
+
+		// Boolean Operator
+
+//		int a=3,b=5,c=3;
+//		System.out.println((a>b)&&(a==c)); //false
+//		System.out.println((a>b)||(a==c)); //true
+//		System.out.println(!(a==c)); //false
+//		System.out.println((a<b)&&(a==c)); //true
+//		System.out.println((a<b)||(a==c)); //true
+
+		// IF-ELSE
+
+//		System.out.println("Enter your age to check whether you are eligible or not?");
+//		int age=sc.nextInt();
+//		if(age>=18) {
+//			System.out.println("Eligible To Vote");
+//		}else {
+//			System.out.println("Not Eligible To Vote");
+//		}
+
+//		System.out.println("Enter your marks?");
+//		int mark = sc.nextInt();
+//		if (mark > 90) {
+//			System.out.println("A Grade");
+//		} else if (mark > 75) {
+//			System.out.println("B Grade");
+//		} else {
+//			System.out.println("C Grade");
+//		}
+		
+		char ch=sc.next().charAt(0);
+		System.out.println("Inputted Character is: "+ch);
+
+	}
+
+}
+
+// Hello Students
+// Hello Students
+// Hello Students
+
+//3
+//45
+//67
